5fa953a9770fcb7feaf75ff5f9f281d12291f7ff
Constadine <58189202+Constadine@users.noreply.github.com>
2021-06-04 16:29:16 +0300

    30

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,19 @@
-import { useState, useEffect } from 'react';
-import BlogList from './BlogList';
+
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => res.json())
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            });
-    }, []); // dependency: run only once initially
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
 
     return (
-
         <div className="home">
-            {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+        { error && <div>{ error }</div> }
+        { isPending && <div>Loading...</div> }
+        { blogs && <BlogList blogs={blogs} /> }
         </div>
-
     );
-}
+    }
+    
 
 export default Home;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link> {/* use 'Link' instead of 'a' for links when using router */}
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
